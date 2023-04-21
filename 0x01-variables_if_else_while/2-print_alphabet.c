cc9b8298b38dd89e2785a24c8a4fd1af063a870b
Akua-95 <serwaa.precious@gmail.com>
2023-04-21 16:56:35 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main	-	prints the alphabet in lowercase
+ * Return:	Always (Success)
+ */
+int	main(void)
+{
+	char	lc;
+
+	for	(lc	=	 'a';	 c <=	'z';	lc++)
+	{
+		putchar(lc);
+	}
+		putchar('\n');
+		return	(0);
+}
+
+
+
