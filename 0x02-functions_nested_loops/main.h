717c21edfc079a54f53f83551f120971ebad9c55
Akua-95 <serwaa.precious@gmail.com>
2023-04-25 08:59:07 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef main.h
+#define main.h
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -12,6 +12,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n)
+void print_to_98(int n);
 
 #endif
