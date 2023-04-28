790215086c7bca41796596409c60fa992312ab1a
Akua-95 <serwaa.precious@gmail.com>
2023-04-27 18:50:17 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,12 @@
 /**
  * _isupper - checks for the lowercase character
  * @c: The character to be checked
- * Return: 1 for uppercase character or 0 for otherwise
+ * Return: 1 for uppercase character or 0 for anything else
  */
 
 int _isupper(int c)
 {
-if (c >= 65 && c >= 90)
+if (c >= 65 && c <= 90)
 
 {
 return (1);
