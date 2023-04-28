fc5aae8a87fbaba64a3eaf3c5a5fee323d14873f
Akua-95 <serwaa.precious@gmail.com>
2023-04-28 05:51:45 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - straight line
+ * @n: Number of times
+ * Return: Always 0 (Success)
+ */
+
+void print_line(int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		_putchar('-');
+	}
+	_putchar('\n');
+}
