370bfb9bc723930beaa0f39815319988e9a2cc62
Akua-95 <serwaa.precious@gmail.com>
2023-05-02 14:38:03 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverse a string
+ * @s: The string
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int length, z, half;
+	
+	char tmp;
+	for (length = 0; s[length] != '\0'; length++)
+	z = 0;
+	half = length / 2;
+
+	while (half ++)
+	{
+		tmp = s[length - z - 1];
+		s[length - z - 1] = s[z];
+		s[z] = temp;
+ 		z++;
+	}
+}	
