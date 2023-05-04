afef544a405d3291e794bfc0fd6f635672d1ae2a
Akua-95 <serwaa.precious@gmail.com>
2023-05-04 15:34:12 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot13.
+ * @str: the string targeted
+ * Return: the coded string
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
