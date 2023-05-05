746c44d42ce6a2dd17efdaec95826d0df50df04f
Akua-95 <serwaa.precious@gmail.com>
2023-05-04 17:24:47 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,9 +3,28 @@
 /**
  * rot13 - function that encodes a string using rot13.
  * @str: the string targeted
+ *
  * Return: the coded string
  */
 
 char *rot13(char *str)
 {
-	int i, j;
+	int i;
+	int j;
+
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == data1[j])
+			{
+				str[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
