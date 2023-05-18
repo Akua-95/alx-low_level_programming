c20f2401d146566c6ce4d5a51e348eab24277c82
Akua-95 <serwaa.precious@gmail.com>
2023-05-18 13:40:16 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,22 +6,23 @@
  * @s1: first string
  * @s2: second string
  * @n: number of bytes
- *
  * Return: If the function fails, it should return NULL
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
 	unsigned int a = 0, b = 0, c = 0, d = 0;
 
-	while (s1[c])
-		c++;
-	while (s2[d])
+	while (s2 && s2[d])
 		d++;
+	while (s1 && s1[c])
+		c++;
 	if (n < d)
 		s = malloc(sizeof(char) * (c + n + 1));
 	else
 		s = malloc(sizeof(char) * (c + d + 1));
+	if (!s)
 		return (NULL);
 	while (a < c)
 	{
@@ -30,10 +31,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	while (n < d && a < (c + n))
 		s[a++] = s2[b++];
-	while (n >= c && a < (c + d))
+	while (n >= d && a < (c + d))
 		s[a++] = s2[b++];
 	s[a] = '\0';
 	return (s);
 }
-
-
