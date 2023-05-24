ad296694d7191d2ae2b75ec74d859d1bdf521afc
Akua-95 <serwaa.precious@gmail.com>
2023-05-24 06:54:36 -0700

    task 3

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int byte, j;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	byte = atoi(argv[1]);
+
+	if (byte < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
+
+	for (j = 0; j < byte; j++)
+	{
+		if (j == byte - 1)
+		{
+		printf("%02hhx\n", arr[j]);
+		break;
+		}
+		printf("%02hhx ", arr[j]);
+	}
+	return (0);
+}
