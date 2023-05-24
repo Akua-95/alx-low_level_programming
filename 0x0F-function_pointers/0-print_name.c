c1df7c0c83e5adedefbc0292923814a8b94d9b9b
Akua-95 <serwaa.precious@gmail.com>
2023-05-24 07:13:23 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,14 +3,15 @@
 
 /**
  * print_name - function that prints a name
- * @name: name of the person
+ * @name: name
  * @f: pointer to function
  * Return: nothing
- */
+ **/
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
+
 	f(name);
 }
