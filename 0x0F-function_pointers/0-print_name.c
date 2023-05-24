da90506c63c9ca4a6b88464730f967854c258513
Akua-95 <serwaa.precious@gmail.com>
2023-05-24 15:47:28 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,15 +3,13 @@
 
 /**
  * print_name - function that prints a name
- * @name: string to add
- * @f: pointer to function
+ * @name: name to print
+ * @f: pointer to function that print name
  * Return: nothing
- **/
-
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-
 	f(name);
 }
