bece6e22d0123c96db73ae20a9a809020a74c6e9
Akua-95 <serwaa.precious@gmail.com>
2023-05-25 09:26:21 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,7 +4,7 @@
 
 /**
  * print_numbers - function that prints numbers, followed by a new line.
- * @separator - string to be printed between numbers
+ * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  * @...:  A variable
  */
@@ -18,14 +18,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (j = 0; j < n; j++)
 	{
-		if (!separator)
 		printf("%d", va_arg(ab, int));
 
-		else if
-			(separator && j == 0);
-		printf("%d", va_arg(ab, int));
-		else
-			printf("%s%d", separator, va_arg(ab, int);
+		if (j != (n - 1) && separator != NULL)
+		printf("%s", separator);
 	}
 	printf("\n");
 	va_end(ab);
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - function that prints anything
+ * @format: list of types of arguments passed to the function
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	char *s, *sep *  = "";
+	int j;
+
+	va_list ab;
+
+	va_start(ab, format);
+
+	if (format)
+	{
+		while (format[j])
+		{
+			switch (format[j])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(ab, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(ab, double));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(ab, int));
+					break;
+				case 's':
+					s = va_arg(ab, char *);
+					if (!s)
+					s = "(nill)";
+					printf("%s%s", sep, s);
+					break;
+				default:
+					j++;
+					continue;
+			}
+
+			sep = ", ";
+			j++;
+		}
+	}
+	printf("\n");
+	va_end(ab);
+}
