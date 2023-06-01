53ff5700831315947cc362193f44942c21928be4
Akua-95 <serwaa.precious@gmail.com>
2023-06-01 11:59:05 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * first - fuction that prints a sentence before main
+ * is executed
+ * Return: Always 0.
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf(" bore my house upon my back!\n");
+}
