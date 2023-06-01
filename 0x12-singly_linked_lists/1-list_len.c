e983c3d220d3b7be5725014a46119a5a0fbe38c9
Akua-95 <serwaa.precious@gmail.com>
2023-06-01 08:27:06 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * list_len - function that returns the number of elements in a linked list_t list
+ * @h: pointer to the list_t
+ *
+ * Return: the number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t z;
+
+	while (h)
+	{
+		z++;
+		h = h->next;
+	}
+	return (z);
+}
