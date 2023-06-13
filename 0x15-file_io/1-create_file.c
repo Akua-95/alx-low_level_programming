ad19b529b609781c757a37fd8296bdb3f77c4fd1
Akua-95 <serwaa.precious@gmail.com>
2023-06-12 17:29:16 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
@@ -24,6 +25,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1 || i == -1)
 		return (-1);
+	
 	close(fd);
 
 	return (1);
